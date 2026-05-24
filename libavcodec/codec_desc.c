558973aa94a3dc5fc1ef0dc75d1d281031db665f
Simon Capriotti <sicapr98@gmail.com>
2026-05-24 15:23:26 +0200

    avcodec: add ATRAC3+ ATS codec ID and descriptor
    Signed-off-by: Simon Capriotti <sicapr98@gmail.com>

--- libavcodec/codec_desc.c
+++ libavcodec/codec_desc.c
@@ -3567,6 +3567,13 @@ static const AVCodecDescriptor codec_descriptors[] = {
         .long_name = NULL_IF_CONFIG_SMALL("CRI AHX"),
         .props     = AV_CODEC_PROP_INTRA_ONLY | AV_CODEC_PROP_LOSSY,
     },
+    {
+        .id        = AV_CODEC_ID_ATRAC3P_ATS,
+        .type      = AVMEDIA_TYPE_AUDIO,
+        .name      = "atrac3plus_ats",
+        .long_name = NULL_IF_CONFIG_SMALL("ATRAC3+ ATS (Adaptive TRansform Acoustic Coding 3+ ATS Syntax)"),
+        .props     = AV_CODEC_PROP_INTRA_ONLY | AV_CODEC_PROP_LOSSY,
+    },
 
     /* subtitle codecs */
     {
